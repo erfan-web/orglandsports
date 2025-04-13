2f6bd3b78a09558bb659575a7c342216c12194e7
Erfan Ahmadi <erfan.dev85@gmail.com>
2025-04-13 18:22:45 +0330

    feat: create project folder structure

---
+++ src/constants/services.js
@@ -0,0 +1,20 @@
+export const servicesData= [
+    {
+      id: 1,
+      image: "https://www.orglandsports.com/Content/Newcss/img/store-icon.png",
+      title: "ارسال به تمامی نقاط کشور",
+      description: "ارسال به کل کشور در کمترین زمان"
+    },
+    {
+      id: 2,
+      image: "https://www.orglandsports.com/Content/Newcss/img/review-icon.png",
+      title: "7 روز ضمانت بازگشت",
+      description: "هفت روز مهلت دارید تا تست بنمایید"
+    },
+    {
+      id: 3,
+      image: "https://www.orglandsports.com/Content/Newcss/img/compare-icon.png",
+      title: "ضمانت اصل بودن کالا",
+      description: "تایید اصالت کالا و گارانتی"
+    }
+  ]
