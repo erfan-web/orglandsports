958621edb85c6f5391437bb0f9cfcc58efdbacfd
Erfan Ahmadi <erfan.dev85@gmail.com>
2025-04-13 18:48:53 +0330

    style(layout): aplly RTL layout

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,10 @@
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-
-createRoot(document.getElementById("root")).render(<App />);
+import { ThemeProvider } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.rtl.min.css";
+createRoot(document.getElementById("root")).render(
+  <ThemeProvider dir="rtl">
+    <App />
+  </ThemeProvider>
+);
