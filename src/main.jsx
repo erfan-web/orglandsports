a33e46ace33d70e8136b49cd92751c9fe7fcf7ee
Erfan Ahmadi <erfan.dev85@gmail.com>
2025-04-15 21:39:36 +0330

    style(scss): customize Bootstrap variables, fonts, and reset styles
    - Updated SCSS variables for color palette and typography - Added custom font and applied global
    resets (margin, padding) - Overrode Bootstrap container and container-fluid classes  - Created
    custom dropdown component styles  - Added component-level resets via component.scss

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import "./index.css";
 import App from "./App.jsx";
 import { ThemeProvider } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.rtl.min.css";
+import "./assets/styles/custom.scss";
 createRoot(document.getElementById("root")).render(
   <ThemeProvider dir="rtl">
     <App />
