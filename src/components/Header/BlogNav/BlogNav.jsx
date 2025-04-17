d00dfef55dde4344c4a86e20d99e1a03acf8911e
Erfan Ahmadi <erfan.dev85@gmail.com>
2025-04-17 12:45:16 +0330

    fix(blognav): close toggle menu on resize and actived backdrop

--- src/components/Header/BlogNav/BlogNav.jsx
+++ src/components/Header/BlogNav/BlogNav.jsx
@@ -3,30 +3,62 @@ import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import Offcanvas from "react-bootstrap/Offcanvas";
 import { Link, NavLink, useLocation } from "react-router-dom";
-import { FaAngleDown, FaAngleLeft } from "react-icons/fa6";
-import { Dropdown } from "react-bootstrap";
 import { memo, useEffect, useMemo, useState } from "react";
 import BtnShop from "../../BtnShop/BtnShop";
 import SortBox from "./SortBox/SortBox";
 import DropDownNav from "./DropDownNav/DropDownNav";
-
 function BlogNav() {
   const expand = "lg";
   const location = useLocation();
-  const [isExpand, setIsExpand] = useState(window.innerWidth >= 992);
+  const [isExpand, setIsExpand] = useState(null);
+  const [isOffcanvasOpen, setIsOffcanvasOpen] = useState(false);
   useEffect(() => {
-    const handleResize = () => {
-      setIsExpand(window.innerWidth >= 992);
+    const mediaQuery = window.matchMedia("(min-width: 992px)");
+
+    const handleMediaChange = (e) => {
+      const isNowDesktop = e.matches;
+      setIsExpand(isNowDesktop);
+      if (isNowDesktop && isOffcanvasOpen) {
+        setIsOffcanvasOpen(false);
+      }
     };
 
-    window.addEventListener("resize", handleResize);
-    return () => window.removeEventListener("resize", handleResize);
-  }, []);
+    handleMediaChange(mediaQuery);
+
+    mediaQuery.addEventListener("change", handleMediaChange);
+    return () => mediaQuery.removeEventListener("change", handleMediaChange);
+  }, [isOffcanvasOpen]);
 
   const sortBox = useMemo(() => {
     return location.pathname === "/" && isExpand ? <SortBox /> : null;
   }, [location.pathname, isExpand]);
 
+  const navLinks = (
+    <>
+      <NavLink to="/" className="nav-link">
+        صفحه اصلی
+      </NavLink>
+      <DropDownNav isExpand={isExpand} />
+      <NavLink to="/AllBrand" className="nav-link">
+        برند ها
+      </NavLink>
+      <NavLink to="/Articles" className="nav-link">
+        دانستنی ها
+      </NavLink>
+      <NavLink to="/About" className="nav-link">
+        درباره ما
+      </NavLink>
+      <NavLink to="/Account/Newuser" className="nav-link">
+        ثبت نام
+      </NavLink>
+      <NavLink to="/Account" className="nav-link">
+        پروفایل من
+      </NavLink>
+      <NavLink to="/Contact" className="nav-link">
+        ارتباط با ما
+      </NavLink>
+    </>
+  );
   return (
     <>
       <Navbar key={expand} expand={expand} className="blogNav">
@@ -34,7 +66,10 @@ function BlogNav() {
           <Link to={`/cart`} className="d-none d-lg-block ">
             <BtnShop>سبد خرید {`(0)`}</BtnShop>
           </Link>
-          <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-${expand}`} />
+          <Navbar.Toggle
+            aria-controls={`offcanvasNavbar-expand-${expand}`}
+            onClick={() => setIsOffcanvasOpen((prev) => !prev)}
+          />
           <Navbar.Brand className="d-lg-none">
             فروشگاه اینترنتی اورجلند
           </Navbar.Brand>
@@ -42,37 +77,17 @@ function BlogNav() {
             id={`offcanvasNavbar-expand-${expand}`}
             aria-labelledby={`offcanvasNavbarLabel-expand-${expand}`}
             placement="end"
-            backdrop={false}
-            className={"offCanvasNav"}
+            backdrop={isOffcanvasOpen}
+            show={isOffcanvasOpen}
+            className={`offCanvasNav ${isOffcanvasOpen ? "d-flex" : "d-none"}`}
+            onHide={() => setIsOffcanvasOpen(false)}
           >
             <Offcanvas.Header closeButton></Offcanvas.Header>
             <Offcanvas.Body>
-              <Nav className="flex-grow-1">
-                <NavLink to="/" className="nav-link">
-                  صفحه اصلی
-                </NavLink>
-                <DropDownNav isExpand={isExpand} />
-                <NavLink to="/AllBrand" className="nav-link">
-                  برند ها
-                </NavLink>
-                <NavLink to="/Articles" className="nav-link">
-                  دانستنی ها
-                </NavLink>
-                <NavLink to="/About" className="nav-link">
-                  درباره ما
-                </NavLink>
-                <NavLink to="/Account/Newuser" className="nav-link">
-                  ثبت نام
-                </NavLink>
-                <NavLink to="/Account" className="nav-link">
-                  پروفایل من
-                </NavLink>
-                <NavLink to="/Contact" className="nav-link">
-                  ارتباط با ما
-                </NavLink>
-              </Nav>
+              <Nav className="flex-grow-1">{navLinks} </Nav>
             </Offcanvas.Body>
           </Navbar.Offcanvas>
+          <Nav className="flex-grow-1 d-none d-lg-flex">{navLinks}</Nav>
           {sortBox}
         </Container>
       </Navbar>
