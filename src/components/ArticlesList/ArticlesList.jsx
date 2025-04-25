e2c5111c7ad358fa059a357cffcd2fabb616598d
Erfan Ahmadi <erfan.dev85@gmail.com>
2025-04-25 16:05:00 +0330

    feat: create ArticlesList component

---
+++ src/components/ArticlesList/ArticlesList.jsx
@@ -0,0 +1,19 @@
+import { memo } from "react";
+import { Col, Row } from "react-bootstrap";
+import ArticleCard from "../ArticleCard/ArticleCard";
+
+const ArticlesList = ({children}) => {
+  return (
+    <>
+      <h2 className="text-center  mb-5 fs-5 fw-bold">مقالات و دانستنی ها</h2>
+      <Row className="row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-xl-4 gy-5  pb-3">
+        {children.map((article) => (
+          <Col key={article.id}>
+            <ArticleCard {...article} />
+          </Col>
+        ))}
+      </Row>
+    </>
+  );
+};
+export default memo(ArticlesList);
