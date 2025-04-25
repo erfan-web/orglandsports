6fe62c3b0f03d6e3d25072e7802cddb37cf89f07
Erfan Ahmadi <erfan.dev85@gmail.com>
2025-04-25 11:43:14 +0330

    refactor(routehelpers): add generateArticleDetailRoute function and fix previous route helper

--- src/utils/routeHelpers.js
+++ src/utils/routeHelpers.js
@@ -4,11 +4,15 @@ export const generateCategoryRoute = (obj) => {
   const slug = generateSlug(obj.name);
   return `/category/${obj.id}/${slug}`;
 };
-export const generateProductDetailRoute=(id , name) =>{
+export const generateProductDetailRoute = (id, name) => {
   const slug = generateSlug(name);
   return `/ProductDetail/${id}/${slug}`;
-}
-export const productBrandRoute=(brandName)=>{
-  const encodedBrand = encodeURIComponent(brandName)
-  return `/ProductBrands/${encodedBrand}`
-}
+};
+export const productBrandRoute = (brandName) => {
+  const slug = generateSlug(brandName);
+  return `/ProductBrands/${slug}`;
+};
+export const generateArticleDetailRoute = (id, name) => {
+  const slug = generateSlug(name);
+  return `/ArticleDetails/${id}/${slug}`;
+};
