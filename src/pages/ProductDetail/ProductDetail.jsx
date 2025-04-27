c6890bcdabbc8ce943981d8b9a9d3123e5428785
Erfan Ahmadi <erfan.dev85@gmail.com>
2025-04-27 13:15:17 +0330

    feat(productdetail): add articlesSection in productDetail page

--- src/pages/ProductDetail/ProductDetail.jsx
+++ src/pages/ProductDetail/ProductDetail.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useMemo, useState } from "react";
 
 import { useDispatch, useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
@@ -8,13 +8,27 @@ import TabSection from "../../components/TabSection/TabSection";
 import SimilarProducts from "../../components/TabSection/SimilarProducts/SimilarProducts";
 import MessageBox from "../../components/MessageBox/MessageBox";
 import MessageProvider from "./MessageContext";
+import { getShuffledItems } from "../../utils/shuffleHelpers";
+import { fetchArticles } from "../../features/slices/articles";
+import ArticlesList from "../../components/ArticlesList/ArticlesList";
+import { Container } from "react-bootstrap";
 function ProductDetail() {
   const { productId } = useParams();
   const dispatch = useDispatch();
   const { product, status } = useSelector((store) => store.productReducer);
+  const { articles } = useSelector((store) => store.articlesReducer);
+
+  const shuffledArticles = useMemo(() => {
+    if (!articles) return [];
+    return getShuffledItems({
+      items: articles,
+      limit: 4,
+    });
+  }, [articles]);
 
   useEffect(() => {
     dispatch(getProductById({ productId }));
+    dispatch(fetchArticles());
   }, [dispatch, productId]);
 
   const [messageStatus, setMessageStatus] = useState(false);
@@ -36,6 +50,11 @@ function ProductDetail() {
         </MessageProvider>
         <TabSection />
         <SimilarProducts />
+        <section className=" px-4 pt-4" style={{ paddingBottom: "80px" }}>
+          <Container>
+            <ArticlesList>{shuffledArticles}</ArticlesList>
+          </Container>
+        </section>
       </>
     );
 }
