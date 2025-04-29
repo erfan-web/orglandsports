a86138b27f1863cf0d2d9a3f7df449ca9513cddf
Erfan Ahmadi <erfan.dev85@gmail.com>
2025-04-29 20:49:46 +0330

    feat: add another categories section in home page

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,9 +1,9 @@
 import { Col, Container, Row } from "react-bootstrap";
+import { Link } from "react-router-dom";
 import HeroSwiper from "../../components/HeroSwiper/HeroSwiper";
 import ServicesList from "../../components/ServicesList/ServicesList";
 import ShopSlider from "../../components/ShopSlider/ShopSlider";
 import SpetialSection from "../../components/SpetialSection/SpetialSection";
-import { Link } from "react-router-dom";
 import PopularSection from "../../components/PopularSection/PopularSection";
 
 function Home() {
@@ -13,7 +13,6 @@ function Home() {
       <ServicesList />
       <ShopSlider />
       <SpetialSection />
-
       <Container fluid>
         <Row className="row-cols-1 row-cols-sm-2 gy-2">
           <Col className="p-0 shadow order-2 order-sm-1">
@@ -43,6 +42,32 @@ function Home() {
 
       <PopularSection />
 
+      <Container fluid>
+        <Row className="row-cols-1 gy-4">
+          <Col className="p-0 shadow ">
+            <div className="p-3 border border-light-subtle">
+              <Link to={`/category/5/کفش-پیاده-روی`}>
+                <img
+                  src="https://www.orglandsports.com/WebsiteImages/ServiceGroupImages/846827143.JPG"
+                  alt=""
+                  className="w-100"
+                />
+              </Link>
+            </div>
+          </Col>
+          <Col className="p-0 shadow orde-1 order-sm-2">
+            <div className="p-3 border border-light-subtle">
+              <Link to={`/category/2/کفش-فوتسال`}>
+                <img
+                  src="https://www.orglandsports.com/WebsiteImages/ServiceGroupImages/848929074.JPG"
+                  alt=""
+                  className="w-100"
+                />
+              </Link>
+            </div>
+          </Col>
+        </Row>
+      </Container>
     </>
   );
 }
